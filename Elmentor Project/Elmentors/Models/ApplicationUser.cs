9a4ce5fa12a56b5dae143e670ace3b25569057a6
AhmedAbdelhamid19 <ahmedaways200219@gmail.com>
2025-05-03 13:52:18 +0300

    delete project

--- Elmentor Project/Elmentors/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Elmentors.Models
-{
-    public class ApplicationUser: IdentityUser<int>
-    {
-        public string? Address { get; set; }
-    }
-}
